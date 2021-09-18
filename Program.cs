359804ee85e1b922a062369e3a77140c7bc31d04
Deus <87348669+FalconOfTheSkys@users.noreply.github.com>
2021-09-18 16:26:26 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Encryption
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            debug();
+        }
+        
+        static void debug()
+        {
+            Console.WriteLine("Enter text to encrypt:");
+            string msg = Console.ReadLine();
+            encrypt(msg);
+            Console.WriteLine();
+            Console.WriteLine("Enter the message above to decrypt it:");
+            string encmsg = Console.ReadLine();
+            decrypt(encmsg);
+            Console.ReadKey();
+        }
+
+        static void encrypt(string msg)
+        {
+            char[] msgchars = msg.ToCharArray();
+
+            for(int i = 0; i < msgchars.Length; i++)
+            {
+                msgchars[i]++;
+                Console.Write(msgchars[i]);
+            }
+        }
+
+        static void decrypt(string encmsg)
+        {
+            char[] msgchars = encmsg.ToCharArray();
+
+            for(int i = 0; i<msgchars.Length; i++)
+            {
+                msgchars[i]--;
+                Console.Write(msgchars[i]);
+            }
+        }
+    }
+}
